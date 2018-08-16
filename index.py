cec9cc42b36513b04a9d69ffbe5950087174c6dd
shipxing <434475464@qq.com>
2018-08-17 07:29:23 +0800

    dcf

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
+import math
+
 def dcf(cash, growthRate, susGrowthRate, discountRate, year)
-    (cash * (1 + riseRate))/(1 + discountRate)
+    total = 0
+    for i in range(1, year)
+        total += (cash * math.pow((1 + riseRate), i))/math.pow((1 + discountRate), i)
+
+    total = (total * (1 + susGrowthRate) * (1 + discountRate))/(discountRate - susGrowthRate)
+    total  = total/math.pow((1 + discountRate), year + 1)
+
+    print(total)
